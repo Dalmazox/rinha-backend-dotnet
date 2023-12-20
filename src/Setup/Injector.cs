289deaea70081708cc84f3d7bfa3b22a26a88389
Dalmazox <kelvi.dalmazo@gmail.com>
2023-12-19 22:09:31 -0300

    chore: first commit with project structure

---
+++ src/Setup/Injector.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Setup;
+
+public static class Injector
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
---
+++ src/WebApi/Controllers/PersonsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("/pessoas")]
+public class PersonsController : ControllerBase
+{
+    [HttpPost] 
+    public IActionResult CreateAsync()
+    {
+        return Created("", null);
+    }
+
+    [HttpGet("{id:guid:required}")]
+    public IActionResult FindByIdAsync(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{t:required}")]
+    public IActionResult FindByTerm(string t)
+    {
+        return Ok();
+    }
+
+    [HttpGet("contagem-pessoas")]
+    public IActionResult Count()
+    {
+        return Ok();
+    }
+    
+}
